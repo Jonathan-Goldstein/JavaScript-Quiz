09eb618388e55d296a69eca27876f84f01617b63
Jonathan Goldstein <jonathangoldsteine@gmail.com>
2021-01-20 18:19:54 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+var startBtn = document.getElementById("startBtn");
+var submitBtn = document.querySelector("button.submitBtn")
+var secondsLeft = (questions.length * 20 + 1);
+var timerElement = document.getElementById("timer");
+var submitScoreElement = document.querySelector("#submit-score");
+var userScoreElement = document.getElementById("user-score");
+var userNameInput;
+var questionHead = document.getElementById("questions");
+var answerChoices = document.getElementById("answers");
+
+var questionNumber = -1;
+var answer;
