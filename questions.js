65c99218278996123a53f9fd20d4fc063182ca19
Jonathan Goldstein <jonathangoldsteine@gmail.com>
2021-01-20 18:14:10 -0500

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,39 @@
+var questions = [
+    {
+        title: "How many bagels are in a bakers dozen?",
+        choices: ["12", "5", "100", "13"],
+        answer: "13"
+    },
+
+    {
+        title: "Who is the chosen one in J.K. Rowling's book about magical english wizards?",
+        choices: ["Hermione Granger", "Ronald Weasely", "Harry Potter", "Albus Dumbledore"],
+        answer: "Harry Potter"
+    },
+
+    {
+        title: "How many stars are on the American flag?",
+        choices: ["100", "30", "50", "45"],
+        answer: "50"
+    },
+
+    {
+        title: "Which of the following numbers are Pi",
+        choices: ["2.98", "9.87", "3.14", "6.34"],
+        answer: "3.14"
+    },
+
+    {
+        title: "Which American President issued the Emancipation Proclamation?",
+        choices: ["George Washington", "Abraham Lincoln", "Bill Clinton", "William Taft"],
+        answer: "Abraham Lincoln"
+    },
+
+    {
+        title: "What does html stand for?",
+        choices: ["Hypertext Markup Language", "Hyperlinks and Text Markup Language", "Hyper Text Manipulation Language", "Home Tool Manipulation"],
+        answer: "Hypertext Markup Language"
+    },
+
+
+];
