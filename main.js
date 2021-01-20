8910b55eed3fc07704b976cc0389e786496ab66f
Jonathan Goldstein <jonathangoldsteine@gmail.com>
2021-01-20 18:25:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,27 @@ var answerChoices = document.getElementById("answers");
 
 var questionNumber = -1;
 var answer;
+function startTimer() {
+ 
+    document.getElementById("home").classList.add('d-none');
+    document.getElementById("quiz").classList.remove('d-none');
+
+    // timer set and begins 120 second countdown
+    setTimer();
+
+    // create questions to display
+    makeQuestions();
+}
+
+function setTimer() {
+
+    var countdown = setInterval(function () {
+        secondsLeft--;
+        timerElement.textContent = "Time: " + secondsLeft;
+
+        if (secondsLeft === 0 || questionNumber === questions.length) {
+            clearInterval(countdown);
+            setTimeout(displayScore, 500);
+        }
+    }, 1000);
+}
